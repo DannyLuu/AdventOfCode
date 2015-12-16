a534a9953987fcbf5949e2debfc6e7cffef68544
Danny Luu <hisoka365@gmail.com>
2015-12-16 14:57:24 -0500

    Day02 Part 2 complete.

--- src/advent02/day02.java
+++ src/advent02/day02.java
@@ -24,11 +24,30 @@ import java.io.IOException;
  * 1 square foot of slack, for a total of 43 square feet.
  * 
  * All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
+ * 
+ * --- Part Two ---
+ * 
+ * The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry about 
+ * the length they need to order, which they would again like to be exact.
+ * 
+ * The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter 
+ * of any one face. Each present also requires a bow made out of ribbon as well; the feet of ribbon required 
+ * for the perfect bow is equal to the cubic feet of volume of the present. Don't ask how they tie the bow, though; 
+ * they'll never tell.
+ * 
+ * For example:
+ * A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of 
+ * ribbon for the bow, for a total of 34 feet.
+ * A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet 
+ * of ribbon for the bow, for a total of 14 feet.
+ * 
+ * How many total feet of ribbon should they order?
  * @author Hisoka
  *
  */
 public class day02 {
-	private static int totalSqFt = 0;
+	private static int totalWrappingPaper = 0;
+	private static int totalRibbon = 0;
 	
 	public static void main(String args[]) {
 		File file = new File("/Users/Hisoka/Documents/workspace/adventofcode/src/advent02/dimensions.txt");
@@ -39,7 +58,8 @@ public class day02 {
 			e.printStackTrace();
 		}
 		
-		System.out.println("The elves should order: " + totalSqFt + "ft");
+		System.out.println("The elves should order: " + totalWrappingPaper + " ft of wrapping paper");
+		System.out.println("The elves should order: " + totalRibbon + " ft of ribbons");
 	}
 
 	private static void readDimensions(File file) throws IOException {
@@ -48,7 +68,8 @@ public class day02 {
 		String line = null;
 		while ((line = bufferedReader.readLine()) != null) {
 			// Perform action
-			totalSqFt += printWrappingPaper(line);
+			totalWrappingPaper += printWrappingPaper(line);
+			totalRibbon += printRibbon(line);
 		}
 		bufferedReader.close();
 	}
@@ -83,5 +104,31 @@ public class day02 {
 		}
 		
 		return area + min;
+	}
+	
+	private static int printRibbon(String line) {
+		int ribbonLength = 0;
+		String [] dimensions = parseDimensions(line);
+		
+		int l = Integer.parseInt(dimensions[0]);
+		int w = Integer.parseInt(dimensions[1]);
+		int h = Integer.parseInt(dimensions[2]);
+		
+		return smallestPerimeter(l, w, h) + volumeOfPresent(l, w, h);		
+	}
+	private static int smallestPerimeter(int l, int w, int h) {
+		int min = (l + w) * 2;
+		if (min > (w + h) * 2) {
+			min = (w + h) * 2;
+		}
+		if (min > (l + h) * 2) {
+			min = (l + h) * 2;
+		}
+		
+		return min;
+	}
+	
+	private static int volumeOfPresent(int l, int w, int h) {
+		return l * w * h;
 	}
 }
